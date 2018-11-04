909b78464f3efa0e4a1c1693c45ab6fd2d22d52e
txg5214 <715709084@qq.com>
2018-11-04 19:32:51 +0800

    update shell

--- bin/build.sh
+++ bin/build.sh
@@ -1,9 +1,9 @@
 #!bin/sh
 
-docker login --username=txg5214 registry.cn-hangzhou.aliyuncs.com
+docker login --username=txg5214 registry.cn-shenzhen.aliyuncs.com
 
 docker build -t vinda-article .
 
-docker tag vinda-article:latest registry.cn-hangzhou.aliyuncs.com/vinda/vinda-article:1.0.0
+docker tag vinda-article:latest registry.cn-shenzhen.aliyuncs.com/vinda/vinda-article:1.0.0
 
-docker push registry.cn-hangzhou.aliyuncs.com/vinda/vinda-article:1.0.0
+docker push registry.cn-shenzhen.aliyuncs.com/vinda/vinda-article:1.0.0
